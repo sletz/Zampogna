aa3b4a85ee20efd88fbbc786429440408299c6d8
Stephane Letz <letz@grame.fr>
2022-06-01 18:35:23 +0200

    Add Faust export.

--- test/EQregalia/build.sh
+++ test/EQregalia/build.sh
@@ -1,1 +1,3 @@
 node ../../src/zampogna.js -i EQregalia -c low,med,high -t cpp -d false EQregalia.crm
+node ../../src/zampogna.js -i EQregalia -c low,med,high -t faust -d false EQregalia.crm
+
--- test/stereoVolControl/build.sh
+++ test/stereoVolControl/build.sh
@@ -1,1 +1,2 @@
 node ../../src/zampogna.js -i volControl  -c vol1,vol2 -t cpp -d false volControl.crm
+node ../../src/zampogna.js -i volControl  -c vol1,vol2 -t faust -d false volControl.crm
